e73f76e9ac48d4a9dddf10e1f968a979fc81707d
Ajithkumar20050103 <ajiaji88707@gmail.com>
2025-09-02 14:30:03 +0530

    validation completed successfully

--- POUS.c
+++ POUS.c
@@ -1,15 +1,12 @@
 void MAIN_init__(MAIN *data__, BOOL retain) {
-  __INIT_LOCATED(BOOL,__IX0_0,data__->STARTBUTTON,retain)
-  __INIT_LOCATED_VALUE(data__->STARTBUTTON,__BOOL_LITERAL(FALSE))
-  __INIT_LOCATED(BOOL,__QX0_0,data__->MOTOR,retain)
-  __INIT_LOCATED_VALUE(data__->MOTOR,__BOOL_LITERAL(FALSE))
+  __INIT_VAR(data__->X,__BOOL_LITERAL(FALSE),retain)
 }
 
 // Code part
 void MAIN_body__(MAIN *data__) {
   // Initialise TEMP variables
 
-  __SET_LOCATED(data__->,MOTOR,,__GET_LOCATED(data__->STARTBUTTON,));
+  __SET_VAR(data__->,X,,__BOOL_LITERAL(TRUE));
 
   goto __end;
 
